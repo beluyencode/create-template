7be077872818ec8e1da28c7583c523664044999b
beluyencode <longlc956@gmail.com>
2023-06-11 01:16:45 +0700

    Update create-template.component.ts

--- src/app/create-template/create-template.component.ts
+++ src/app/create-template/create-template.component.ts
@@ -57,25 +57,6 @@ export class CreateTemplateComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.createTemplateService.getTemplateSample().subscribe((res: any) => {
-      const newTemplateGroup = new TemplateGroup('');
-      const newTemplate = new Template('', 0);
-      this.createTemplateService.listElement = res.map((ele: any) => {
-        if (ele.data) {
-          return newTemplateGroup.clone().convertType({
-            ...ele,
-            data: ele.data.map((ele2: any) => {
-              return newTemplate.clone().convertType(ele2);
-            })
-          });
-        } else {
-          return newTemplate.clone().convertType(ele);
-        }
-      })
-      console.log(this.createTemplateService.listElement);
-
-      this.createTemplateService.load_list_element.next(this.createTemplateService.listElement);
-    })
     this.createTemplateService.listen_id_template().subscribe((res: string) => {
       if (res !== '' && typeof res === 'string') {
         this.createTemplateService.getData(res).subscribe((res: any) => {
@@ -85,10 +66,20 @@ export class CreateTemplateComponent implements OnInit {
               this.createTemplateService.active_template.next(null);
             }
             if (res?.data?.config?.listElement) {
+              const newTemplateGroup = new TemplateGroup('');
               const newTemplate = new Template('', 0);
               this.createTemplateService.listElement = res?.data?.config?.listElement.map((ele: any) => {
-                return newTemplate.clone().convertType(ele);
-              });
+                if (ele.data) {
+                  return newTemplateGroup.clone().convertType({
+                    ...ele,
+                    data: ele.data.map((ele2: any) => {
+                      return newTemplate.clone().convertType(ele2);
+                    })
+                  });
+                } else {
+                  return newTemplate.clone().convertType(ele);
+                }
+              })
               this.createTemplateService.load_list_element.next(this.createTemplateService.listElement);
             }
           }
